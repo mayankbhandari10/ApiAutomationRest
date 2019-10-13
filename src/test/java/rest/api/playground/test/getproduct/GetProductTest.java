569e1274d994c85373ca6a94226f6cd33f47da36
Mayank bhandari <mayank10oct@>
2019-10-14 03:07:44 +0530

    Added the dependencies

--- src/test/java/rest/api/playground/test/getproduct/GetProductTest.java
+++ src/test/java/rest/api/playground/test/getproduct/GetProductTest.java
@@ -1,5 +1,36 @@
 package rest.api.playground.test.getproduct;
+import org.testng.annotations.Test;
 
-public class GetProductTest {
+import io.restassured.RestAssured;
 
-}
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import rest.api.playground.testbase.TestBase;
+public class GetProductTest extends TestBase {
+
+	public GetProductTest() throws Exception {
+		super();
+		
+	}
+
+	@Test(priority=1, description="verify the login")
+	public void fn_verify_login() throws Exception{
+		try
+		{
+			RestAssured.baseURI="";
+			 
+	        
+	
+	       
+		}
+		catch(Exception e)
+		{
+		  throw e;	
+		}
+	}
+
+	
+	}
+	
+	
+
