d96cab8790268be710d6b683fe4c47614ef6332e
Mayank bhandari <mayank10oct@>
2019-10-19 18:36:52 +0530

    added the log$j properties

--- src/main/java/rest/api/playground/testbase/TestBase.java
+++ src/main/java/rest/api/playground/testbase/TestBase.java
@@ -3,17 +3,21 @@ package rest.api.playground.testbase;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
 public class TestBase {
 	public static Properties prep;
 	String path = System.getProperty("user.dir");
 	private final String ProperfilePath= path+"\\\\src\\main\\java\\rest\\api\\playground\\config\\config.properties";
-	
+
+	public 	Logger log = Logger.getLogger("devpinoyLogger");
+
 	public TestBase() throws Exception 
 	{
 		try
 		{
 			//getClass().getResource("config.properties"); // you get an URL, then openStream it
-            System.out.println(ProperfilePath);
+			System.out.println(ProperfilePath);
 			prep= new Properties();
 			FileInputStream ip = new FileInputStream(ProperfilePath);
 			prep.load(ip);
--- src/test/java/rest/api/playground/test/getproduct/GetProductTest.java
+++ src/test/java/rest/api/playground/test/getproduct/GetProductTest.java
@@ -1,7 +1,9 @@
 package rest.api.playground.test.getproduct;
 import org.testng.annotations.Test;
-
 import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -10,32 +12,56 @@ public class GetProductTest extends TestBase {
 
 	public GetProductTest() throws Exception {
 		super();
-		
+
 	}
 
 	@Test(priority=1, description="verify the login")
 	public void fn_verify_login() throws Exception{
+		int id=43900;
+		log.info("*****************id is*******************"+id);
+		String baseuri=prep.getProperty("uri");
 		try
 		{
-			int id=43900;
-			String baseuri=prep.getProperty("uri");
-			RestAssured.baseURI=baseuri;
-			given().
-			when().get("/products").then().assertThat().statusCode(200).and().
-			body("data[0].id".trim(),equalTo(id)).and().
-			body("data[0].name",equalTo("Duracell - AAA Batteries (4-Pack)"));
-	        
-	
-	       
+		
+		RestAssured.baseURI=baseuri;
+		log.info("**********************log started***************************");
+		log.info("Base uri is"+baseuri);
+		given().
+		when().get("/products").then().assertThat().statusCode(200).and().
+		body("data[0].id".trim(),equalTo(id)).and().
+		body("data[0].name",equalTo("Duracell - AAA Batteries (4-Pack)"));
+		
 		}
 		catch(Exception e)
 		{
-		  throw e;	
+			throw e;	
 		}
 	}
 
-	
+	@Test(priority=2, description="verify the get request")
+	public void fn_verify_getRequest() throws Exception{
+		int id=43900;
+		
+		log.info("*****************id is*******************"+id);
+		String baseuri=prep.getProperty("uri");
+		try
+		{
+		
+			RequestSpecification httpRequest = RestAssured.given();
+			Response response = httpRequest.request(Method.GET, "/products");
+			String responseBody = response.getBody().asString();
+			log.info("************respone**************"+responseBody);
+		
+		
+		}
+		catch(Exception e)
+		{
+			throw e;	
+		}
 	}
-	
-	
+
+
+}
+
+
 
