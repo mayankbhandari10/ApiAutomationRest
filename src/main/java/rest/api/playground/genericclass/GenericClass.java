f189da378ff9d7fa043111ea0b65ce4293c8fc34
robin singh <mayank10oct@yahoo.com>
2020-04-22 11:34:27 -0700

    Added the testdata using json

--- src/main/java/rest/api/playground/genericclass/GenericClass.java
+++ src/main/java/rest/api/playground/genericclass/GenericClass.java
@@ -1,5 +1,10 @@
 package rest.api.playground.genericclass;
+import java.io.File;
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 import io.restassured.path.json.JsonPath;
 
@@ -21,6 +26,39 @@ public class GenericClass {
 			throw e;
 		}
 
+	}
+
+	@SuppressWarnings({ "unused", "unchecked" })
+	public
+	static void fn_Read_data_from_json(String filename) throws Exception
+	{
+		try
+		{
+			JSONParser jsonParser = new JSONParser();
+			FileReader reader = new FileReader(filename);
+			//Read JSON file
+			Object obj = jsonParser.parse(reader);
+			JSONArray array = new JSONArray();
+			array.add(obj);
+			int size=array.size();
+			System.out.println(size);
+			System.out.println(obj); //This prints the entire json file
+			for (int i=0;i<size;i++)
+			{
+				JSONObject code = (JSONObject) array.get(i);
+				JSONObject Status = (JSONObject) code.get("Status");
+				String Unauthorized = (String) Status.get("Unauthorized");
+				String Forbidden = (String) Status.get("Forbidden");
+				System.out.println(Status); //This prints each data in the block
+
+
+			}
+
+		}
+		catch(Exception e)
+		{
+			throw e;
+		}
 	}
 
 
--- src/test/java/rest/api/playground/test/addproduct/test/AddProductTest.java
+++ src/test/java/rest/api/playground/test/addproduct/test/AddProductTest.java
@@ -9,7 +9,12 @@ import rest.api.playground.genericclass.GenericClass;
 import rest.api.playground.test.addproduct.AddProductPayload;
 import rest.api.playground.testbase.TestBase;
 import com.fasterxml.jackson.databind.ObjectMapper;
+@SuppressWarnings("unused")
 public class AddProductTest extends TestBase {
+	String path = System.getProperty("user.dir");
+	String FinalTestDat=path+"\\TestData\\Statuscode.json";
+
+
 
 	AddProductPayload Ad=new AddProductPayload();
 	//AddProductPayload Ad=new AddProductPayload("string", "string", 50, 5, "string", "string", "string", "string", "string", "string");
@@ -19,7 +24,7 @@ public class AddProductTest extends TestBase {
 
 	}
 
-	ObjectMapper Obj = new ObjectMapper(); 
+	//ObjectMapper Obj = new ObjectMapper(); 
 	/*@Test(priority=1, description="Add a new Product and verify it")
 	public void fn_verify_post_Request() throws Exception{
 		String json = Obj.writeValueAsString(Ad);
@@ -45,25 +50,27 @@ public class AddProductTest extends TestBase {
 			throw e;	
 		}
 	}
-	 */ 	@Test(priority=2, description="Add a new Product and verify it using json simple libraray")
-
-	 private void fn_verify_postRequest_using_json_simple_lib() throws Exception
-	 {
-		 try
-		 {
-			 RestAssured.baseURI=baseuri;
-			 RequestSpecification request = RestAssured.given().contentType("application/json");
-			 String request_body=Ad.fn_Create_Add_product();
-			 System.out.println(request_body);
-			 request.body(request_body);
-			 Response response = request.post("/products");
-			 int statusCode = response.getStatusCode();
-			 System.out.println("The status code recieved: " + statusCode);
-			 System.out.println("Response body: " + response.body().asString());
-		 }
-		 catch(Exception e)
-		 {
-			 throw e; 	
-		 }
-	 }
+	 */ 	
+	@Test(priority=2, description="Add a new Product and verify it using json simple libraray")
+	private void fn_verify_postRequest_using_json_simple_lib() throws Exception
+	{
+		try
+		{
+			GenericClass.fn_Read_data_from_json(FinalTestDat);
+			RestAssured.baseURI=baseuri;
+			RequestSpecification request = RestAssured.given().contentType("application/json");
+			String request_body=Ad.fn_Create_Add_product();
+			System.out.println(request_body);
+			request.body(request_body);
+			Response response = request.post("/products");
+			int statusCode = response.getStatusCode();
+			log.info("*************The status code recieved:**************" + statusCode);
+			log.info("**********************Response body:******************* " + response.body().asString());
+			GenericClass.fn_Read_data_from_json(FinalTestDat);			 
+		}
+		catch(Exception e)
+		{
+			throw e; 	
+		}
+	}
 }
